e896f94893a9b5f60f05600d0d631b2cdde2be55
Antonio Francisco <afpb04@hotmail.com>
2020-11-29 21:55:49 -0300

    add signin page and signup

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <h1>GoDelivery</h1>
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="login">
+        <FiLogIn />
+        Criar Conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
