8738e3ee5ff4ceb11c3d0f71dab0377be0b31720
Antonio Francisco <afpb04@hotmail.com>
2021-01-07 13:05:49 -0300

    add dashborad

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,100 @@
 import React from 'react';
+import { isToday } from 'date-fns';
+
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Orders,
+  NextOrders,
+  Section,
+  Order,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const { signOut, user } = useAuth();
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoDelivery" />
+
+          <Profile>
+            <img
+              src="https://avatars2.githubusercontent.com/u/15930084?s=460&u=00e846aa1cc7943cca6769dccbe301ef0815f1c8&v=4"
+              alt={user.name}
+            />
+
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+      <Content>
+        <Orders>
+          <h1>Pedidos</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 05</span>
+            <span>Segunda-terça</span>
+          </p>
+          <NextOrders>
+            <strong>Pedido da vez</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/15930084?s=460&u=00e846aa1cc7943cca6769dccbe301ef0815f1c8&v=4"
+                alt="Pedido"
+              />
+              <strong>X-buger</strong>
+              <span>
+                <FiClock />
+                22:59
+              </span>
+            </div>
+          </NextOrders>
+          <Section>
+            <strong>Próximos Pedidos</strong>
+            <Order>
+              <span>
+                <FiClock />
+                23:30
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/15930084?s=460&u=00e846aa1cc7943cca6769dccbe301ef0815f1c8&v=4"
+                  alt="Pedido"
+                />
+                <strong>X-buger</strong>
+              </div>
+            </Order>
+            <Order>
+              <span>
+                <FiClock />
+                23:30
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/15930084?s=460&u=00e846aa1cc7943cca6769dccbe301ef0815f1c8&v=4"
+                  alt="Pedido"
+                />
+                <strong>X-buger</strong>
+              </div>
+            </Order>
+          </Section>
+        </Orders>
+      </Content>
+    </Container>
+  );
 };
 export default Dashboard;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,13 @@
+/* eslint-disable camelcase */
 import React, { useCallback, useRef } from 'react';
-import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import {
+  FiArrowLeft,
+  FiMail,
+  FiUser,
+  FiLock,
+  FiBriefcase,
+  FiFileText,
+} from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
@@ -21,6 +29,9 @@ interface SignUpFormData {
   name: string;
   email: string;
   password: string;
+  company_name: string;
+  cnpj: string;
+  company_id: string;
 }
 
 const SignUp: React.FC = () => {
@@ -33,6 +44,8 @@ const SignUp: React.FC = () => {
       try {
         formRef.current?.setErrors({});
         const schema = Yup.object().shape({
+          company_name: Yup.string().required('Nome da Empresa obrigatório'),
+          cnpj: Yup.string().required('CNPJ é obrigatório'),
           name: Yup.string().required('Nome obrigatório'),
           email: Yup.string()
             .required('E-mail obrigatório')
@@ -42,8 +55,17 @@ const SignUp: React.FC = () => {
         await schema.validate(data, {
           abortEarly: false,
         });
+        const response = await api.post('/companies', {
+          company_name: data.company_name,
+          cnpj: data.cnpj,
+        });
 
-        await api.post('/users', data);
+        await api.post('/users', {
+          name: data.name,
+          email: data.email,
+          password: data.password,
+          company_id: response.data.id,
+        });
 
         history.push('/');
 
@@ -77,6 +99,13 @@ const SignUp: React.FC = () => {
 
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça seu cadastro</h1>
+
+            <Input
+              name="company_name"
+              icon={FiBriefcase}
+              placeholder="Nome da empresa"
+            />
+            <Input name="cnpj" icon={FiFileText} placeholder="CNPJ" />
 
             <Input name="name" icon={FiUser} placeholder="Nome" />
             <Input name="email" icon={FiMail} placeholder="E-mail" />
